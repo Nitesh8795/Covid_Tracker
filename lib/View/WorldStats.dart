51e689bb092240706087b49a94a8156f82e516cd
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2022-12-13 01:05:46 +0530

    Widget added for displaying world data

--- lib/View/WorldStats.dart
+++ lib/View/WorldStats.dart
@@ -8,12 +8,11 @@ class WorldStatus extends StatefulWidget {
   State<WorldStatus> createState() => _WorldStatusState();
 }
 
-class _WorldStatusState extends State<WorldStatus> with TickerProviderStateMixin
-{
-  late final AnimationController _controller = AnimationController(
-    duration: const Duration(seconds: 5),
-    vsync: this
-  )..repeat();
+class _WorldStatusState extends State<WorldStatus>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: const Duration(seconds: 5), vsync: this)
+        ..repeat();
 
   @override
   void dispose() {
@@ -25,7 +24,6 @@ class _WorldStatusState extends State<WorldStatus> with TickerProviderStateMixin
     Color(0xff4285F4),
     Color(0xff1aa260),
     Color(0xffde5246),
-
   ];
 
   @override
@@ -37,14 +35,10 @@ class _WorldStatusState extends State<WorldStatus> with TickerProviderStateMixin
           child: Column(
             children: [
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.04,
+                height: MediaQuery.of(context).size.height * 0.02,
               ),
               PieChart(
-                  dataMap: const{
-                    "Total" : 300,
-                    "Recovered" : 200,
-                    "Death" : 100
-                  },
+                dataMap: const {"Total": 300, "Recovered": 200, "Death": 100},
                 chartType: ChartType.ring,
                 legendOptions: const LegendOptions(
                   legendPosition: LegendPosition.left,
@@ -52,9 +46,49 @@ class _WorldStatusState extends State<WorldStatus> with TickerProviderStateMixin
                 animationDuration: const Duration(milliseconds: 2000),
                 colorList: colorList,
               ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * 0.06),
+                child: Card(
+                  child: Column(
+                    children: [
+                      ReusableRow(title: "Nitesh", value: '200'),
+                      ReusableRow(title: "Nitesh", value: '200'),
+                      ReusableRow(title: "Nitesh", value: '200'),
+                      ReusableRow(title: "Nitesh", value: '200'),
+                      ReusableRow(title: "Nitesh", value: '200'),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ReusableRow extends StatelessWidget {
+  String title, value;
+
+  ReusableRow({Key? key, required this.title, required this.value})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 5, right: 10, left: 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Text(title), Text(value)],
+          ),
+          SizedBox(
+            height: 7,
+          ),
+          Divider(),
+        ],
       ),
     );
   }
