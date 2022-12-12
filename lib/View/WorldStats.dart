b9d964b5e4ff1f70dd333e513973eed1620cf854
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2022-12-13 00:35:11 +0530

    Pie Chart visual effects completed

--- lib/View/WorldStats.dart
+++ lib/View/WorldStats.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class WorldStatus extends StatefulWidget {
   const WorldStatus({Key? key}) : super(key: key);
@@ -7,9 +8,54 @@ class WorldStatus extends StatefulWidget {
   State<WorldStatus> createState() => _WorldStatusState();
 }
 
-class _WorldStatusState extends State<WorldStatus> {
+class _WorldStatusState extends State<WorldStatus> with TickerProviderStateMixin
+{
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 5),
+    vsync: this
+  )..repeat();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  final colorList = const <Color>[
+    Color(0xff4285F4),
+    Color(0xff1aa260),
+    Color(0xffde5246),
+
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.04,
+              ),
+              PieChart(
+                  dataMap: const{
+                    "Total" : 300,
+                    "Recovered" : 200,
+                    "Death" : 100
+                  },
+                chartType: ChartType.ring,
+                legendOptions: const LegendOptions(
+                  legendPosition: LegendPosition.left,
+                ),
+                animationDuration: const Duration(milliseconds: 2000),
+                colorList: colorList,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
